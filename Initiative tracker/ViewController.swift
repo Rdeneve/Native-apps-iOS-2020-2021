f6760c89946d9f5be4f7001b676b9a1526e17742
Robbe De Neve <rdeneve00@gmail.com>
2020-12-09 11:41:35 +0100

    Initial Commit

---
+++ Initiative tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Initiative tracker
+//
+//  Created by Robbe De Neve on 09/12/2020.
+//  Copyright © 2020 Robbe De Neve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
